908b47eb97a6d18cce6abafbe0b6063d93565ee7
Till Brede <tb@symcon.de>
2021-10-11 14:00:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# VariablenVergleich
+
+Folgende Module beinhaltet das VariablenVergleich Repository:
+
+- __VariablenVergleich__ ([Dokumentation](VariablenVergleich))  
+	Das VariablenVergleichsmodul ermöglicht es aus zwei Variablen eine Punktwolke zu erstellen und mithilfe von linearer Regression vergleichbar zu machen .
---
+++ VariablenVergleich/module.php
@@ -0,0 +1,376 @@
+<?php
+
+declare(strict_types=1);
+
+include_once __DIR__ . '/libs/WebHookModule.php';
+    class VariablenVergleich extends WebHookModule
+    {
+        public function __construct($InstanceID)
+        {
+            parent::__construct($InstanceID, 'linear-regression/' . $InstanceID);
+        }
+
+        public function Create()
+        {
+            //Variable settings
+            $this->RegisterPropertyInteger('XVariable', 0);
+            $this->RegisterPropertyInteger('YVariable', 0);
+            $this->RegisterPropertyInteger('AggregationLevel', 1);
+            $this->RegisterPropertyString('StartDate', '{"year":2021,"month":1,"day":1}');
+            $this->RegisterPropertyString('EndDate', '{"year":2021,"month":1,"day":1}');
+            
+            //Chart settings
+            $this->RegisterPropertyInteger('AxisMinorStep', 1);
+            $this->RegisterPropertyInteger('AxisMajorStep', 5);
+            $this->RegisterPropertyString('ChartFormat', 'svg');
+            $this->RegisterPropertyInteger('ChartWidth', 1000);
+            $this->RegisterPropertyInteger('ChartHeight', 500);
+            
+            
+            $this->RegisterPropertyString('Chart', '');
+            
+            $this->RegisterVariableString('ChartSVG', $this->Translate('Chart'), '~HTMLBox');
+        }
+
+        public function Destroy()
+        {
+            //Never delete this line!
+            parent::Destroy();
+        }
+
+        public function ApplyChanges()
+        {
+            //Never delete this line!
+            parent::ApplyChanges();
+            
+            if (!@$this->GetIDForIdent('ChartPNG')) {
+                $mediaID = IPS_CreateMedia(1);
+                IPS_SetIdent($mediaID, 'ChartPNG');
+                IPS_SetName($mediaID, 'Chart');
+                IPS_SetParent($mediaID, $this->InstanceID);
+                $this->UpdateFormField('Chart', 'mediaID', $mediaID);
+            }
+            $this->UpdateChart();
+        }
+
+        public function GetConfigurationForm()
+        {
+            $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+            
+            $charts = $this->generateChart();
+            if ($charts != null) {
+                if ($this->ReadPropertyString('ChartFormat') == 'svg') {
+                    $form['elements'][0]['items'][1]['image'] = "data:image/svg+xml;utf8," . $charts['SVG'];
+                } else {
+                    $form['elements'][0]['items'][1]['image'] = "data:image/png;base64," . $charts['PNG'];
+                }
+            }
+
+            return json_encode($form);
+        }
+
+        public function UpdateChart()
+        {
+            $charts = $this->generateChart();
+            if ($charts == null) {
+                return;
+            }
+            $svg = $charts['SVG'];
+            $png = $charts['PNG'];
+            //Force update
+            if ($this->ReadPropertyString('ChartFormat') == 'svg') {
+                $this->UpdateFormField('Chart', 'image', "data:image/svg+xml;utf8," . $svg);
+                $this->SetValue('ChartSVG', "<div style=\"background-color:white; width:" . $this->ReadPropertyInteger('ChartWidth') . "px\">$svg</div>");
+                IPS_SetHidden($this->GetIDForIdent('ChartPNG'), true);
+                IPS_SetHidden($this->GetIDForIdent('ChartSVG'), false);
+            } else {
+                $mediaID = $this->GetIDForIdent('ChartPNG');
+                IPS_SetMediaFile($mediaID, $mediaID . '.png', false);
+                IPS_SetMediaContent($mediaID, $png);
+                $this->UpdateFormField('Chart', 'mediaID', $mediaID);
+                IPS_SetHidden($this->GetIDForIdent('ChartPNG'), false);
+                IPS_SetHidden($this->GetIDForIdent('ChartSVG'), true);
+            }
+        }
+
+        protected function ProcessHookData()
+        {
+            if ($this->ReadPropertyString('ChartFormat') == 'svg') {
+                header('Content-Type: image/svg+xml');
+                header('Content-Length: ' . strlen($this->GetBuffer('ChartSVG')));
+                echo $this->GetBuffer('ChartSVG');
+            } else {
+                header('Content-Type: image/png');
+                header('Content-Length: ' . strlen(base64_decode($this->GetBuffer('ChartPNG'))));
+                echo base64_decode($this->GetBuffer('ChartPNG'));
+            }
+        }
+
+        public function generateChart()
+        {
+            $xVariableId = $this->ReadPropertyInteger('XVariable');
+            $yVariableId = $this->ReadPropertyInteger('YVariable');
+            if ($xVariableId != 0 && $yVariableId != 0) {
+                $archiveID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
+                
+                $startDate = $this->dateTimeToTimestamp('StartDate');
+                $endDate = $this->dateTimeToTimestamp('EndDate');
+                $rawX = AC_GetAggregatedValues($archiveID, $xVariableId, $this->ReadPropertyInteger('AggregationLevel'), $startDate, $endDate, 0);
+                $xVarValues = [];
+                foreach ($rawX as $dataset) {
+                    $xVarValues[] = $dataset['Avg'];
+                }
+                $valuesX = array_reverse($xVarValues);
+                
+                $rawY = AC_GetAggregatedValues($archiveID, $yVariableId, $this->ReadPropertyInteger('AggregationLevel'), $startDate, $endDate, 0);
+                $yVarValues = [];
+                foreach ($rawY as $dataset) {
+                    $yVarValues[] = $dataset['Avg'];
+                }
+                $valuesY = array_reverse($yVarValues);
+                if (count($valuesX) != count($valuesY)) {
+                    $this->SetStatus(200);
+                    // The amount of values is not the same for both axis
+                    return null;
+                } elseif (count($valuesY) <= 1) {
+                    $this->SetStatus(201);
+                    // The count of values is zero or one which leads to an error in the linear regression
+                    return null;
+                }
+            } else {
+                //No vars selected
+                $this->SetStatus(202);
+                return null;
+            }
+            //Set the status to active if there are no errors
+            $this->SetStatus(102);
+                
+            $customWidth = $this->ReadPropertyInteger('ChartWidth');
+            $customHeight = $this->ReadPropertyInteger('ChartHeight');
+
+            $chartXOffset = 50;
+            $chartYOffset = 50;
+            $xRange = $customWidth - $chartXOffset;
+            $width = $xRange + $chartXOffset;
+            
+            $yRange = $customHeight - $chartYOffset;
+            $height = $yRange + $chartYOffset;
+
+            $image=imagecreate($width, $height);
+            $font = 5;
+
+            //PNG colors
+            $white=imagecolorallocate($image, 255, 255, 255);
+            $textWhite = imagecolorallocate($image, 254, 254, 254);
+            $black=imagecolorallocate($image, 0, 0, 0);
+            $textColor = $black;
+            $blue=imagecolorallocate($image, 0, 0, 225);
+            $green=imagecolorallocate($image, 0, 255, 0);
+            $red=imagecolorallocate($image, 255, 0, 0);
+            imagecolortransparent($image, $white);
+                      
+            $yFloor = 0;
+            $xFloor = 0;
+            $yCeil = ceil(max($valuesY));
+            $xCeil = ceil(max($valuesX));
+
+            $getXValue = function ($x) use ($xRange, $valuesX, $chartXOffset, $xFloor, $customWidth, $xCeil) {
+                $minimum = min($valuesX) < 0 ? min($valuesX) : 0;
+                return intval(($chartXOffset - 1) + $x * (($customWidth - $chartXOffset * 2) / ($xCeil - $minimum)));
+            };
+
+            $getYValue = function ($y) use ($valuesY, $yRange, $chartYOffset, $yFloor, $yCeil) {
+                $minimum = 0;//min($valuesY) < 0 ? min($valuesY) : 0;
+                return intval(($yRange + $chartYOffset) - $chartYOffset - $y * (($yRange + $chartYOffset - $chartYOffset * 2) / ($yCeil - $minimum)) - 1);
+            };
+            
+           
+            $svg = '<svg version="1.1" ';
+            $svg .= 'width= "' . $width . '" height="' . $height . '" ';
+            $svg .= 'xmlns="http://www.w3.org/2000/svg"> ';
+
+            //Linear regression
+            $lineParameters = $this->computeVariablenVergleichParameters($valuesX, $valuesY);
+            $maxX = max($valuesX);
+            $maxY = max($valuesY);
+            imageline($image, $getXValue($xFloor), $getYValue($lineParameters[0]), $getXValue($maxX), intval($getYValue($lineParameters[0] + ($lineParameters[1] * $maxX))), $red);
+            $svg .= $this->drawLine($getXValue($xFloor), $getYValue($lineParameters[0]), $getXValue($maxX), intval($getYValue($lineParameters[0] + ($lineParameters[1] * $maxX))), 'red');
+
+
+            //Y AXIS
+            imageline($image, $getXValue($xFloor), intval($yRange - 1), $getXValue($xFloor), $getYValue($yCeil), $textColor);
+            $svg .= $this->drawLine($getXValue($xFloor), $yRange - 1, $getXValue($xFloor), $getYValue($yCeil), 'black');
+            $svg .= $this->drawPolygon([
+                [$getXValue($xFloor), $getYValue($yCeil) - 2],
+                [$getXValue($xFloor) + 4, $chartYOffset + 1 + 8],
+                [$getXValue($xFloor) - 4, $chartYOffset + 1 + 8],
+            ]);
+
+
+            //Y number line
+            $axisLabelOffset = 5;
+            $svgOffset = 5;
+            $charWidth = imagefontwidth($font);
+            for ($i = $yFloor; $i < ceil($maxY); $i++) {
+                $offset = intval(imagefontheight($font) / 2);
+                if ($i % $this->ReadPropertyInteger('AxisMajorStep') == 0) {
+                    imageline($image, $getXValue($xFloor) - 4, $getYValue($i), $getXValue($xFloor) + 4, $getYValue($i), $textColor);
+                    imagestring($image, $font, $getXValue($xFloor) - ($charWidth * strlen(strval($i))) -  $axisLabelOffset, $getYValue($i) - $offset, strval($i), $textColor);
+                    $svg .= $this->drawLine($getXValue($xFloor) - 4, $getYValue($i), $getXValue($xFloor) + 4, $getYValue($i), 'black');
+                    $svg .= $this->drawText($getXValue($xFloor) - $svgOffset, $getYValue($i), 'black', 15, strval($i), true);
+                } elseif ($i % $this->ReadPropertyInteger('AxisMinorStep') == 0) {
+                    imageline($image, $getXValue($xFloor) - 2, $getYValue($i), $getXValue($xFloor) + 2, $getYValue($i), $textColor);
+                    $svg .= $this->drawLine($getXValue($xFloor) - 2, $getYValue($i), $getXValue($xFloor) + 2, $getYValue($i), 'black');
+                }
+            }
+
+            $axisNameLabelOffset = 25;
+            //Y label
+            $charHeight = imagefontheight($font);
+            $yLabelText = $this->getAxisLabel('YVariable');
+            imagestringup($image, 5, $getXValue($xFloor) - imagefontheight($font) - $axisLabelOffset - ($charWidth * strlen(strval(ceil($maxY)))), intval($customHeight/2 + (($charWidth * strlen($yLabelText)) / 2)), $yLabelText, $textColor);
+            $svg .= $this->drawAxisTitle(1 + $svgOffset, $customHeight / 2, 'black', $yLabelText, true);
+            
+            //X AXIS
+            imageline($image, $getXValue($xFloor), $getYValue($yFloor), $width - $chartXOffset - 1, $getYValue($yFloor), $textColor);
+            $svg .= $this->drawLine($getXValue($xFloor), $getYValue($yFloor), $width - $chartXOffset - 1, $getYValue($yFloor), 'black');
+            $svg .= $this->drawPolygon([
+                [$getXValue($xCeil) + 4, $getYValue($yFloor)],
+                [$getXValue($xCeil)   - 8, $getYValue($yFloor) + 4],
+                [$getXValue($xCeil)   - 8, $getYValue($yFloor) - 4],
+            ]);
+            
+            
+            //X number line
+            for ($i = $xFloor; $i < $xCeil; $i++) {
+                if ($i % $this->ReadPropertyInteger('AxisMajorStep') == 0) {
+                    $valueString = strval($i);
+                    $offset = intval((strlen($valueString) * $charWidth) / 2);
+                    imageline($image, $getXValue($i), $getYValue($yFloor) - 4, $getXValue($i), $getYValue($yFloor) + 4, $textColor);
+                    imagestring($image, $font, $getXValue($i) - $offset, $getYValue($yFloor) + 10, strval($i), $textColor);
+                    $svg .= $this->drawLine($getXValue($i), $getYValue($yFloor) - 4, $getXValue($i), $getYValue($yFloor) + 4, 'black');
+                    $svg .= $this->drawText($getXValue($i), $getYValue($yFloor) + $svgOffset, 'black', 15, $valueString, false);
+                } elseif ($i % $this->ReadPropertyInteger('AxisMinorStep') == 0) {
+                    imageline($image, $getXValue($i), $getYValue($yFloor) - 2, $getXValue($i), $getYValue($yFloor) + 2, $textColor);
+                    $svg .= $this->drawLine($getXValue($i), $getYValue($yFloor) - 2, $getXValue($i), $getYValue($yFloor) + 2, 'black');
+                }
+            }
+            
+            //X label
+            $xLabelText = $this->getAxisLabel('XVariable');
+            imagestring($image, 5, $customWidth/2 - intval((strlen($xLabelText) * $charWidth) / 2), $getYValue($yFloor) + $axisNameLabelOffset, $xLabelText, $textColor);
+            $svg .= $this->drawAxisTitle($customWidth/2, $customHeight - $svgOffset, 'black', $xLabelText);
+
+
+            for ($i = 0; $i < count($valuesY); $i++) {
+                $xValue = $getXValue($valuesX[$i]);
+                $yValue = $getYValue($valuesY[$i]);
+                $this->pngPoint($image, $xValue, $yValue, 5, $blue);
+                $svg .= $this->drawCircle($xValue, $yValue, 2, 'blue');
+            }
+
+            $svg .= '</svg>';
+
+            //Base64 encode image
+            ob_start();
+            imagepng($image);
+            $imageData = ob_get_contents();
+            ob_end_clean();
+            $base64 = base64_encode($imageData);
+            $this->SetBuffer('ChartPNG', $base64);
+            $this->SetBuffer('ChartSVG', $svg);
+
+            return [
+                'SVG' => $svg,
+                'PNG' => $base64
+            ];
+        }
+
+        private function dateTimeToTimestamp($property)
+        {
+            $date = json_decode($this->ReadPropertyString($property), true);
+            $timeStamp = strtotime(sprintf('%s.%s.%s', $date['day'], $date['month'], $date['year']));
+            return $timeStamp;
+        }
+
+        private function pngPoint($image, $x, $y, $radius, $color)
+        {
+            imagefilledellipse($image, $x, $y, $radius, $radius, $color);
+        }
+
+        private function drawText($x, $y, $color, $size, $text, $vertical, $orientation = '')
+        {
+            $anchor = $vertical ? 'end': 'middle';
+            $baseline = $vertical ? 'central' : 'hanging';
+            $style = $orientation == 'vertical' ? " transform=\"rotate(-90 $x $y)\"" : '';
+            return "<text x=\"$x\" y=\"$y\" font-size=\"medium\" text-anchor=\"$anchor\" alignment-baseline=\"$baseline\" fill=\"$color\"$style font-family=\"Roboto\">$text</text>";
+        }
+
+        private function drawAxisTitle($x, $y, $color, $text, $vertical = false)
+        {
+            $anchor = 'middle';
+            $baseline = $vertical ? 'hanging' : 'baseline';
+            $transform = $vertical == 'vertical' ? " transform=\"rotate(-90 $x $y)\"" : '';
+            return "<text x=\"$x\" y=\"$y\" font-size=\"large\" text-anchor=\"$anchor\" alignment-baseline=\"$baseline\" fill=\"$color\"$transform font-family=\"Roboto\">$text</text>";
+        }
+        
+        private function drawChartTitle($x, $y, $size, $color, $text)
+        {
+            return "<text x=\"$x\" y=\"$y\" font-size=\"$size\" text-anchor=\"middle\" fill=\"$color\" font-family=\"Roboto\">$text</text>";
+        }
+
+        private function getAxisLabel(string $variableProperty)
+        {
+            $variableID = $this->ReadPropertyInteger($variableProperty);
+            $variable = IPS_GetVariable($variableID);
+            $profileName = $variable['VariableProfile'] ? $variable['VariableProfile'] : $variable['VariableCustomProfile'];
+            $profile = IPS_GetVariableProfile($profileName);
+            $suffix = $profile['Suffix'];
+            return utf8_decode(IPS_GetName($variableID) . ' in' . $suffix);
+        }
+
+        private function drawCircle($x, $y, $radius, $color)
+        {
+            return "<circle cx=\"$x\" cy=\"$y\" r=\"$radius\" fill=\"$color\" />";
+        }
+
+        private function drawLine($x1, $y1, $x2, $y2, $color)
+        {
+            return "<line x1=\"$x1\" y1=\"$y1\" x2=\"$x2\" y2=\"$y2\" stroke=\"$color\" />";
+        }
+
+        private function drawPolygon(array $points)
+        {
+            $pointsString = '';
+            foreach ($points as $point) {
+                $pointsString .= $point[0] . ',' . $point[1] .' ';
+            }
+            $pointsString = substr($pointsString, 0, strlen($pointsString) - 1);
+            $string = "<polygon points=\"$pointsString\"/>";
+            return $string;
+        }
+
+        //Formular from example at https://de.wikipedia.org/wiki/Lineare_Einfachregression https://wikimedia.org/api/rest_v1/media/math/render/svg/31c4eb5b4144dc6ff9364337f902c9ca65623039
+        private function computeVariablenVergleichParameters(array $valuesX, array $valuesY)
+        {
+            $averageX = array_sum($valuesX) / count($valuesX);
+            $averageY = array_sum($valuesY) / count($valuesY);
+            $beta1Denominator = 0;
+            $beta1Divider = 0;
+            for ($i = 0; $i < count($valuesX); $i++) {
+                $beta1Denominator += ($valuesX[$i] - $averageX) * ($valuesY[$i] - $averageY);
+                $beta1Divider += pow(($valuesX[$i] - $averageX), 2);
+            }
+            $beta1 = $beta1Denominator / $beta1Divider;
+
+            $beta0 = $averageY - ($beta1 * $averageX);
+            
+            return [$beta0, $beta1];
+        }
+
+        public function Download()
+        {
+            $charts = $this->generateChart();
+            echo '/hook/linear-regression/' . $this->InstanceID;
+        }
+    }
